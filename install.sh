bdacab7b549925bd460960b5fbdf937455525637
JTekk <jtekk@jtekk.dev>
2026-01-02 19:14:51 -0700

    fixing helium script

--- install.sh
+++ install.sh
@@ -9,34 +9,34 @@ fi
 
 REAL_USER=${SUDO_USER:-$(whoami)}
 
-# echo "Setting up Repos!"
-# sleep 2
-# source ./install/repos.sh
-#
-# echo "Setting up Core items!"
-# sleep 2
-# source ./install/core.sh
-#
-# echo "Setting up system"
-# sleep 2
-# source ./install/cli.sh
-# source ./install/tuis.sh
-# source ./install/tools.sh
-# source ./install/fonts.sh
-# source ./install/mango-wm.sh
-# source ./install/apps/mangowc.sh
-# source ./install/guis.sh
-# source ./install/apps.sh
-# source ./install/extras.sh
-#
-# echo "Setting up Thinkpad"
-# source ./install/thinkpad.sh
-#
-# echo "Setting up seatd and dbus!"
-# sleep 2
-# usermod -aG _seatd $REAL_USER
-# ln -s /etc/sv/seatd /var/service/
-# ln -s /etc/sv/dbus /var/service/
+echo "Setting up Repos!"
+sleep 2
+source ./install/repos.sh
+
+echo "Setting up Core items!"
+sleep 2
+source ./install/core.sh
+
+echo "Setting up system"
+sleep 2
+source ./install/cli.sh
+source ./install/tuis.sh
+source ./install/tools.sh
+source ./install/fonts.sh
+source ./install/mango-wm.sh
+source ./install/apps/mangowc.sh
+source ./install/guis.sh
+source ./install/apps.sh
+source ./install/extras.sh
+
+echo "Setting up Thinkpad"
+source ./install/thinkpad.sh
+
+echo "Setting up seatd and dbus!"
+sleep 2
+usermod -aG _seatd $REAL_USER
+ln -s /etc/sv/seatd /var/service/
+ln -s /etc/sv/dbus /var/service/
 
 sudo -u "$REAL_USER" ./install/apps/ble.sh
 sudo -u "$REAL_USER" ./install/flatpaks.sh
@@ -46,4 +46,4 @@ sudo -u "$REAL_USER" ./install/apps/helium.sh
 echo "DONE!!!"
 echo "Welcome to the VOID!"
 sleep 2
-#reboot
+reboot
--- install/apps/helium.sh
+++ install/apps/helium.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p $HOME/AppImages
+mkdir -p ~/AppImages
 wget -P ~/AppImages https://github.com/imputnet/helium-linux/releases/download/0.7.7.1/helium-0.7.7.1-x86_64.AppImage
 mv ~/AppImages/helium-0.7.7.1-x86_64.AppImage ~/AppImages/helium.appimage
 chmod a+x ~/AppImages/helium.appimage
