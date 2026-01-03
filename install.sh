ae9d054ec6da3d5d9ebbb737f7b731ad86a8fda0
JTekk <jtekk@jtekk.dev>
2026-01-02 19:59:01 -0700

    adding xbps-pkgdb to avoid confusing as orphans

--- install.sh
+++ install.sh
@@ -23,8 +23,7 @@ source ./install/cli.sh
 source ./install/tuis.sh
 source ./install/tools.sh
 source ./install/fonts.sh
-source ./install/mango-wm.sh
-source ./install/apps/mangowc.sh
+source ./install/desktop-env.sh
 source ./install/guis.sh
 source ./install/apps.sh
 source ./install/extras.sh
@@ -37,11 +36,14 @@ sleep 2
 usermod -aG _seatd $REAL_USER
 ln -s /etc/sv/seatd /var/service/
 ln -s /etc/sv/dbus /var/service/
+ln -s /etc/sv/elogind /var/service/
+ln -s /etc/sv/bluetoothd /var/service/
+ln -s /etc/sv/swayosd-libinput-backend /var/service/
 
 sudo -u "$REAL_USER" ./install/apps/ble.sh
-sudo -u "$REAL_USER" ./install/flatpaks.sh
 sudo -u "$REAL_USER" ./install/apps/brew.sh
 sudo -u "$REAL_USER" ./install/apps/helium.sh
+sudo -u "$REAL_USER" ./install/apps/flatpaks.sh
 
 echo "DONE!!!"
 echo "Welcome to the VOID!"
